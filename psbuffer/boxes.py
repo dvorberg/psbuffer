9b8164df96b03f48ee915458ecd5318322285f7b
Diedrich Vorberg <diedrich@tux4web.de>
2023-01-27 23:57:46 +0100

    Moved try and test code to examples/

--- psbuffer/boxes.py
+++ psbuffer/boxes.py
@@ -235,33 +235,3 @@ class RasterImage(EPSBox):
 
         super().__init__(self.raster_image_buffer(pil_image),
                          bb, document_level, border, clip, comment)
-
-
-if __name__ == "__main__":
-    from io import BytesIO
-
-    from PIL import Image
-
-    from .measure import mm
-    from .dsc import Document, Page
-
-    document = Document()
-    page = document.append(Page())
-    canvas = page.append(Canvas(mm(18), mm(18), page.w-mm(36), page.h-mm(36)))
-
-
-    filepath = "/Users/diedrich/Desktop/Test.eps"
-    test_eps = open(filepath, "br")
-    img = EPSImage(test_eps, document_level=False, border=True,
-                   comment=filepath)
-    img.fit(canvas)
-
-    filename = "/Users/diedrich/Desktop/dings.png"
-    img = RasterImage(Image.open(filename),
-                      border=True, comment=filename)
-    #img.fit(canvas)
-
-    fp = BytesIO()
-    document.write_to(fp)
-
-    print(fp.getvalue().decode("iso-8859-1"))
