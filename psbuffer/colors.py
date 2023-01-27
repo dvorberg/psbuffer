637e9bdc76e862e6379d4dc17dd4e0dbdba6a84e
Diedrich Vorberg <diedrich@tux4web.de>
2023-01-27 23:57:48 +0100

    Moved try and test code to examples/

--- psbuffer/colors.py
+++ psbuffer/colors.py
@@ -134,23 +134,3 @@ class TransparentColor(Color):
         return False
 
 transparent = TransparentColor()
-
-######################################################################
-# When I grow up, I’ll be a unit test!
-
-if __name__ == "__main__":
-    from io import BytesIO
-    from .base import PSBuffer
-
-    b = PSBuffer()
-
-    for a in ("white", "black", "red", "green", "blue"):
-        b.print(globals().get(a), f"% {a}")
-
-    b.print()
-    b.print(WebColor("#FFBE33"), b'% WebColor("#FFBE33"))')
-
-    fp = BytesIO()
-    b.write_to(fp)
-
-    print(fp.getvalue().decode("ascii"))
