5e52751f55284ae9f2f12d1464ea9c913942e98c
Diedrich Vorberg <diedrich@tux4web.de>
2023-02-08 00:20:36 +0100

    Initial commit (here) Font management works!

---
+++ psbuffer/fontbase/__init__.py
@@ -0,0 +1,4 @@
+from .encoding_tables import ( glyph_name_to_codepoint,
+                               codepoint_to_glyph_name,
+                               encoding_tables, )
+from .fontbase import Font, GlyphMetric, FontMetrics, FontResourceSection
