b7d02ca3bec2e5159bced067478f82ffd686e53a
Diedrich Vorberg <diedrich@tux4web.de>
2023-02-08 00:19:56 +0100

    Moved glyph_name_to_codepoint and codepoint_to_glyph_name to fontbase.

--- psbuffer/type1/__init__.py
+++ psbuffer/type1/__init__.py
@@ -23,29 +23,4 @@
 ##  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ##
 ##  I have added a copy of the GPL in the file gpl.txt.
-
-import os.path as op, re
-
-glyph_name_to_codepoint = {}
-codepoint_to_glyph_name = {}
-
-#entry_re = re.compile(r"([A-Za-z0-9]+),([0-9a-f]+)")
-with open(op.join(op.dirname(__file__), "glyph_name_to_unicode.csv")) as fp:
-    for line_no, line in enumerate(fp.readlines()):
-        line = line.rstrip()
-
-        if not line or line[0] == "#":
-            continue
-
-        #match = entry_re.match(line)
-        #if match is None:
-        #    raise IOError(f"Illegal entry on line {line_no+1}: {repr(line)}")
-        #else:
-        #    glyph_name, codepoint = match.groups()
-        #    codepoint = int(codepoint, 16)
-
-        glyph_name, codepoint = line.split(",")
-        codepoint = int(codepoint, 16)
-
-        glyph_name_to_codepoint[glyph_name] = codepoint
-        codepoint_to_glyph_name[codepoint] = glyph_name
+from .type1 import Type1
