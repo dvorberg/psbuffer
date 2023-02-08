3374c465925f9755296f22059f3dc91519059015
Diedrich Vorberg <diedrich@tux4web.de>
2023-02-08 12:50:56 +0100

    Moved to fonts.

--- psbuffer/fontbase/__init__.py
+++
@@ -1,4 +0,0 @@
-from .encoding_tables import ( glyph_name_to_codepoint,
-                               codepoint_to_glyph_name,
-                               encoding_tables, )
-from .fontbase import Font, GlyphMetric, FontMetrics, FontResourceSection
